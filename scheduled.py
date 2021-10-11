2d29d8f5c858269f39f94cf54911ff9e5aa708c1
Teal Larson <teal.larson@grouparoo.com>
2021-10-10 20:29:25 -0400

    shuffle channel members, run daily at 9

---
+++ scheduled.py
@@ -0,0 +1,57 @@
+import os
+import time
+import logging
+import random
+import schedule
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+
+def shuffle_users(slack_client):
+
+  try:
+    user_list = slack_client.users_list(
+      channel='#general'
+    )
+
+  except SlackApiError as err:
+    logging.error('Request to Slack API Failed: {}.'.format(err.response.status_code))
+    logging.error(err.response)
+
+  random.shuffle(user_list["members"])
+  chat_message = ""
+
+  for user in user_list["members"]:
+      chat_message += f'{user["real_name"]} \n'
+
+  return chat_message
+
+
+def send_message(slack_client):
+
+  chat_message = shuffle_users(slack_client)
+
+  try:
+    slack_client.chat_postMessage(
+      channel='#test',
+      text=chat_message
+    )
+  except SlackApiError as err:
+    logging.error('Request to Slack API Failed: {}.'.format(err.response.status_code))
+    logging.error(err.response)
+
+if __name__ == "__main__":
+
+  SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
+  slack_client = WebClient(SLACK_BOT_TOKEN)
+
+  schedule.every().monday.at("9:00").do(lambda: send_message(slack_client))
+  schedule.every().tuesday.at("9:00").do(lambda: send_message(slack_client))
+  schedule.every().wednesday.at("9:00").do(lambda: send_message(slack_client))
+  schedule.every().thursday.at("9:00").do(lambda: send_message(slack_client))
+  schedule.every().friday.at("9:00").do(lambda: send_message(slack_client))
+
+
+  while True:
+    schedule.run_pending()
+    time.sleep(1)
